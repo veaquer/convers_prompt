1c4f9ed03718a0fe134a61f8e5292a397b904afc
veaquer <veaquer@gmail.com>
2024-11-06 20:53:58 +0100

    updated convers to 0.1.9

--- src/app/main_window.rs
+++ src/app/main_window.rs
@@ -2,8 +2,8 @@ use std::sync::{Arc, Mutex};
 
 use convers::convert::magic_convert;
 use egui::{
-    CentralPanel, Color32, FontDefinitions, FontFamily, Frame, Margin, RichText, Rounding,
-    ScrollArea, Stroke, Style, TextStyle, Vec2, Vec2b,
+    Align, CentralPanel, Color32, FontDefinitions, FontFamily, Frame, Layout, Margin, RichText,
+    Rounding, ScrollArea, Stroke, Style, TextStyle, Vec2, Vec2b,
 };
 use tokio::runtime::Runtime;
 
@@ -30,7 +30,7 @@ impl eframe::App for MainWindow {
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let mut style: Style = (*ctx.style()).clone();
-        style.visuals.widgets.inactive.bg_fill = Color32::from_hex("#1e1e2e").unwrap();
+        style.visuals.widgets.inactive.bg_fill = Color32::from_hex("#181825").unwrap();
         style.visuals.widgets.inactive.fg_stroke =
             Stroke::new(1., Color32::from_hex("#cdd6f4").unwrap());
         style.text_styles.insert(
@@ -57,43 +57,55 @@ impl eframe::App for MainWindow {
         CentralPanel::default()
             .frame(
                 Frame::none()
-                    .fill(Color32::from_hex("#1e1e2e").unwrap())
+                    .fill(Color32::from_hex("#181825").unwrap())
                     .inner_margin(Margin::same(5.)),
             )
             .show(ctx, |ui| {
-                let input_id = ui.make_persistent_id("input_field");
+                ui.with_layout(Layout::top_down(Align::Min), |ui| {
+                    let input_id = ui.make_persistent_id("input_field");
 
-                Frame::none()
-                    .inner_margin(Margin::same(2.))
-                    .rounding(Rounding::from(10.))
-                    .fill(Color32::from_hex("#1e1e2e").unwrap())
-                    .stroke(Stroke::new(1., Color32::from_hex("#cba6f7").unwrap()))
-                    .show(ui, |ui| {
-                        let input = egui::TextEdit::singleline(&mut self.prompt)
-                            .frame(false)
-                            .font(TextStyle::Name("big".into()))
-                            .id(input_id);
-                        ui.add_sized(Vec2::new(ui.available_width(), 30.), input);
+                    Frame::none()
+                        .inner_margin(Margin::same(2.))
+                        .rounding(Rounding::from(10.))
+                        .fill(Color32::from_hex("#181825").unwrap())
+                        .stroke(Stroke::new(1., Color32::from_hex("#9399b2").unwrap()))
+                        .show(ui, |ui| {
+                            let input = egui::TextEdit::singleline(&mut self.prompt)
+                                .hint_text(
+                                    RichText::new("Your prompt here...")
+                                        .color(Color32::from_hex("#a6adc8").unwrap()),
+                                )
+                                .frame(false)
+                                .font(TextStyle::Name("big".into()))
+                                .id(input_id);
+                            ui.add_sized(Vec2::new(ui.available_width(), 30.), input);
+                        });
+
+                    ui.memory_mut(|mem| {
+                        mem.request_focus(input_id);
                     });
-
-                ui.memory_mut(|mem| {
-                    mem.request_focus(input_id);
+                    area_rect = Some(ui.min_rect());
+                    ui.add_space(10.);
+                    ScrollArea::vertical()
+                        .animated(true)
+                        .auto_shrink(Vec2b::new(true, false))
+                        .stick_to_right(true)
+                        .stick_to_bottom(false)
+                        .show(ui, |ui| {
+                            let response = self.response.lock().unwrap().clone();
+                            ui.label(
+                                RichText::new(&response)
+                                    .size(24.)
+                                    .color(Color32::from_rgb(205, 214, 244)),
+                            );
+                        });
+                    ui.add_space(10.);
+                    Frame::none().show(ui, |ui| {
+                        ui.set_min_height(100.);
+                        ui.set_width(ui.available_width());
+                        ui.label("Press Enter to submit your prompt.");
+                    });
                 });
-                area_rect = Some(ui.min_rect());
-                ui.add_space(10.);
-                ScrollArea::vertical()
-                    .animated(true)
-                    .auto_shrink(Vec2b::new(true, false))
-                    .stick_to_right(true)
-                    .show(ui, |ui| {
-                        ui.set_width(600.);
-                        let response = self.response.lock().unwrap().clone();
-                        ui.label(
-                            RichText::new(&response)
-                                .size(24.)
-                                .color(Color32::from_rgb(205, 214, 244)),
-                        );
-                    });
             });
 
         ctx.input(|i| {
@@ -103,16 +115,7 @@ impl eframe::App for MainWindow {
                     ctx.send_viewport_cmd(egui::ViewportCommand::Close);
                 });
             }
-            if i.pointer.any_click() {
-                if let Some(rect) = area_rect {
-                    if !rect.contains(i.pointer.interact_pos().unwrap_or_default()) {
-                        let ctx = ctx.clone();
-                        std::thread::spawn(move || {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        });
-                    }
-                }
-            }
+
             if i.key_pressed(egui::Key::Enter) {
                 let prompt = self.prompt.clone();
                 let response = Arc::clone(&self.response);
